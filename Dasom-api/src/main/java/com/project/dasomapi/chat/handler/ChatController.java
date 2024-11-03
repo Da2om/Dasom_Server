f5408610f19d99c00b1c3464cf4c051696a4cb2a
heunseoRyu <heunseo9@gmail.com>
2024-11-04 00:52:46 +0900

    [fix] :: chat
    restcontroller -> Controller

--- Dasom-api/src/main/java/com/project/dasomapi/chat/handler/ChatController.java
+++ Dasom-api/src/main/java/com/project/dasomapi/chat/handler/ChatController.java
@@ -13,7 +13,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ChatController {
 
